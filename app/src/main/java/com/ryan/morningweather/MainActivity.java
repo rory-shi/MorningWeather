65c71fd6e48b831628f8b29dc3d392d5fe36d3ad
Ryan <rory940311@gmail.com>
2016-01-04 11:37:44 +0800

    更新了显示天气的界面。

--- app/src/main/java/com/ryan/morningweather/MainActivity.java
+++ app/src/main/java/com/ryan/morningweather/MainActivity.java
@@ -6,11 +6,17 @@ import android.os.Message;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -22,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     Button showBtn;
     EditText cityText;
     TextView weatherText;
+    ImageButton imgBtn;
     String cityName;
 
 
@@ -35,7 +42,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initEvent() {
-        showBtn.setOnClickListener(new View.OnClickListener() {
+        imgBtn.setOnTouchListener(new View.OnTouchListener() {
+            private float mLastY;
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:{
+                        mLastY = event.getY();
+                        break;
+                    }
+                    case MotionEvent.ACTION_MOVE: {
+                        float y = event.getY();
+                        float alphaDelta = (y - mLastY) / 1000;
+                        float alpha = imgBtn.getAlpha() + alphaDelta;
+                        if (alpha > 1.0) {
+                            alpha = 1.0f;
+                        } else if (alpha < 0.0) {
+                            alpha = 0.0f;
+                        }
+                        imgBtn.setAlpha(alpha);
+                    }
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });
+       /* showBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 cityName = cityText.getText().toString();
@@ -43,9 +76,11 @@ public class MainActivity extends AppCompatActivity {
                 sendRequestWithHttpClient(new HttpCallbackListener() {
                     @Override
                     public void onFinish(String response) {
+                        String qlty =  parseDataWithJsonObject(response);
                         SharedPreferences.Editor editor =
                                 PreferenceManager.getDefaultSharedPreferences(MainActivity.this).edit();
                         editor.putString("weather_data",response.toString());
+                        editor.putString("qlty",qlty);
                         editor.commit();
 
                         runOnUiThread(new Runnable() {
@@ -63,12 +98,28 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
             }
-        });
+        });*/
+    }
+
+    private String parseDataWithJsonObject(String jsonData) {
+        try {
+            JSONObject jsonObject = new JSONObject(jsonData);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather data service 3.0");
+            JSONObject objectData = jsonArray.getJSONObject(0);
+            JSONObject objectAqi = objectData.getJSONObject("aqi");
+            JSONObject objectCity = objectAqi.getJSONObject("city");
+            String qlty = objectCity.getString("qlty");
+            return qlty;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.toString();
+        }
+
     }
 
     private void showWeather() {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
-        cityText.setText(prefs.getString("weather_data",""));
+        weatherText.setText(prefs.getString("qlty",""));
     }
 
     private void sendRequestWithHttpClient(final HttpCallbackListener listener) {
@@ -111,8 +162,9 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void initView() {
-        showBtn = (Button) findViewById(R.id.btn_click_show);
+        /*showBtn = (Button) findViewById(R.id.btn_click_show);
         cityText = (EditText) findViewById(R.id.et_weather_name);
-        weatherText = (TextView) findViewById(R.id.tv_show_weather);
+        weatherText = (TextView) findViewById(R.id.tv_show_weather);*/
+        imgBtn = (ImageButton) findViewById(R.id.img_btn);
     }
 }
