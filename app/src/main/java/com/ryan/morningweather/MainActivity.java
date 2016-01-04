9419f1175e4e46b375f4dde6f57713cd2fe761d3
Ryan <rory940311@gmail.com>
2016-01-04 20:36:54 +0800

    更新了主界面天气温度的进入时刷新以及基于系统的时间

--- app/src/main/java/com/ryan/morningweather/MainActivity.java
+++ app/src/main/java/com/ryan/morningweather/MainActivity.java
@@ -23,14 +23,18 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-    Button showBtn;
-    EditText cityText;
-    TextView weatherText;
-    ImageButton imgBtn;
-    String cityName;
 
+    ImageButton imgBtn;
+    TextView timeTv;
+    TextView dateTv;
+    TextView weatherTv;
+    TextView temperatureTv;
+    TextView provinceTv;
+    TextView cityTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +46,100 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initEvent() {
+        initBackground();
+
+        initData();
+
+    }
+
+    private void initData() {
+        sendRequestWithHttpClient(new HttpCallbackListener() {
+            @Override
+            public void onFinish(String response) {
+                String qlty = getQltyFromJson(response);
+                String weather = getWeatherFromJson(response);
+                String temperature = getTemperatureFromJson(response);
+                SharedPreferences.Editor editor =
+                        PreferenceManager.getDefaultSharedPreferences(MainActivity.this).edit();
+                editor.putString("weather_data", response.toString());
+                editor.putString("qlty", qlty);
+                editor.putString("weather", weather);
+                editor.putString("temperature", temperature);
+                editor.commit();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        showWeather();
+                    }
+                });
+            }
+
+            @Override
+            public void onError(Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+    }
+
+    private void initTime() {
+        Date curDate = new Date(System.currentTimeMillis());
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM月dd日");
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+        String date = dateFormat.format(curDate);
+        String time = timeFormat.format(curDate);
+        SharedPreferences.Editor editor =
+                PreferenceManager.getDefaultSharedPreferences(MainActivity.this).edit();
+        editor.putString("date", date);
+        editor.putString("time", time);
+        editor.commit();
+    }
+
+    private String getTemperatureFromJson(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather data service 3.0");
+            JSONObject objectData = jsonArray.getJSONObject(0);
+            JSONObject now = objectData.getJSONObject("now");
+            return now.getString("tmp");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.toString();
+        }
+    }
+
+    private String getQltyFromJson(String jsonData) {
+        try {
+            JSONObject jsonObject = new JSONObject(jsonData);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather data service 3.0");
+            JSONObject objectData = jsonArray.getJSONObject(0);
+            JSONObject objectAqi = objectData.getJSONObject("aqi");
+            JSONObject objectCity = objectAqi.getJSONObject("city");
+            String qlty = objectCity.getString("qlty");
+            return qlty;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.toString();
+        }
+
+    }
+
+    private String getWeatherFromJson(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather data service 3.0");
+            JSONObject objectData = jsonArray.getJSONObject(0);
+            JSONObject now = objectData.getJSONObject("now");
+            JSONObject cond = now.getJSONObject("cond");
+            return  cond.getString("txt");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.toString();
+        }
+    }
+
+    private void initBackground() {
         imgBtn.setOnTouchListener(new View.OnTouchListener() {
             private float mLastY;
             @Override
@@ -59,8 +157,7 @@ public class MainActivity extends AppCompatActivity {
                             alpha = 1.0f;
                         } else if (alpha < 0.0) {
                             alpha = 0.0f;
-                        }
-                        imgBtn.setAlpha(alpha);
+                        }imgBtn.setAlpha(alpha);
                     }
                     default:
                         break;
@@ -68,67 +165,56 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-       /* showBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cityName = cityText.getText().toString();
-                Toast.makeText(MainActivity.this,cityName,Toast.LENGTH_LONG).show();
-                sendRequestWithHttpClient(new HttpCallbackListener() {
-                    @Override
-                    public void onFinish(String response) {
-                        String qlty =  parseDataWithJsonObject(response);
-                        SharedPreferences.Editor editor =
-                                PreferenceManager.getDefaultSharedPreferences(MainActivity.this).edit();
-                        editor.putString("weather_data",response.toString());
-                        editor.putString("qlty",qlty);
-                        editor.commit();
-
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-
-                                showWeather();
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void onError(Exception e) {
-                        e.printStackTrace();
-                    }
-                });
-            }
-        });*/
     }
 
-    private String parseDataWithJsonObject(String jsonData) {
-        try {
-            JSONObject jsonObject = new JSONObject(jsonData);
-            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather data service 3.0");
-            JSONObject objectData = jsonArray.getJSONObject(0);
-            JSONObject objectAqi = objectData.getJSONObject("aqi");
-            JSONObject objectCity = objectAqi.getJSONObject("city");
-            String qlty = objectCity.getString("qlty");
-            return qlty;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return e.toString();
-        }
 
-    }
 
     private void showWeather() {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
-        weatherText.setText(prefs.getString("qlty",""));
+        String weather = prefs.getString("weather","请等待");
+        String temperature = prefs.getString("temperature","请等待");
+        String date = prefs.getString("date","请等待");
+        String time = prefs.getString("time","请等待");
+        weatherTv.setText(weather);
+        temperatureTv.setText(temperature + " °");
+        dateTv.setText(date);
+        timeTv.setText(time);
+
+        setDefaultBackground(weather);
+    }
+
+    private void setDefaultBackground(String weather) {
+        switch (weather) {
+            case "雾":
+                imgBtn.setAlpha(0.1f);
+                break;
+            case "霾":
+                imgBtn.setAlpha(0.1f);
+                break;
+            case "扬沙":
+                imgBtn.setAlpha(0.1f);
+                break;
+            case "沙尘暴":
+                imgBtn.setAlpha(0.1f);
+                break;
+            case "强沙尘暴":
+                imgBtn.setAlpha(0.1f);
+                break;
+            case "火山灰":
+                imgBtn.setAlpha(0.1f);
+            default:
+                break;
+        }
     }
 
     private void sendRequestWithHttpClient(final HttpCallbackListener listener) {
         new Thread(new Runnable() {
             @Override
             public void run() {
+                initTime();
                 HttpURLConnection connection = null;
                 try {
-                    URL url = new URL("http://apis.baidu.com/heweather/weather/free?" + "city=" + cityName);
+                    URL url = new URL("http://apis.baidu.com/heweather/weather/free?" + "city=" + "hangzhou");
                     connection = (HttpURLConnection) url.openConnection();
                     connection.setRequestMethod("GET");
                     connection.setRequestProperty("apikey","832d7ef40bf399309c9946d515867e80");
@@ -162,9 +248,12 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void initView() {
-        /*showBtn = (Button) findViewById(R.id.btn_click_show);
-        cityText = (EditText) findViewById(R.id.et_weather_name);
-        weatherText = (TextView) findViewById(R.id.tv_show_weather);*/
         imgBtn = (ImageButton) findViewById(R.id.img_btn);
+        timeTv = (TextView) findViewById(R.id.tv_time);
+        dateTv = (TextView) findViewById(R.id.tv_date);
+        weatherTv = (TextView) findViewById(R.id.tv_weather);
+        temperatureTv = (TextView) findViewById(R.id.tv_temperature);
+        provinceTv = (TextView) findViewById(R.id.tv_location_big);
+        cityTv = (TextView) findViewById(R.id.tv_location_small);
     }
 }
